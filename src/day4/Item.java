b5f85c33a0775edd86957d99eac881defede464c
yeofriendship <yeofriendship@naver.com>
2023-02-02 17:17:14 +0900

    2023년 2월 2일

--- src/day4/Item.java
+++ src/day4/Item.java
@@ -1,8 +1,99 @@
 package day4;
 
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
 public class Item {
 	private long code = 0L;
 	private String name = null;
 	private String content = null;
+	private int price = 0;
+	private int quantity = 0;
+	private Date regdate = new Date();
+	private Member memberid = null;
 	
+	// getter, setter, toString 생성하기 => lombok 사용(@~)
+	
+	// 빈 생성자
+	public Item() {
+		super();
+	}
+	
+	// 날짜 필드를 제외한 필드 생성자
+	public Item(long code, String name, String content, int price, int quantity, Member memberid) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.content = content;
+		this.price = price;
+		this.quantity = quantity;
+		this.memberid = memberid;
+	}
+	
+	// 1. 수량이 n개 미만이면 n개로 변경하는 메소드
+	public int underCount(int n) {
+		if (this.quantity < n) {
+			this.quantity = n;
+		}
+		
+		return this.quantity;
+	}
+	
+	// 2. 가격에 할인율만큼 빼서 리턴하는 메소드
+	public int percentPrice(float per) {
+		this.price = (int)(this.price - this.price*per); 
+		
+		return this.price;
+	}
+	
+	// 3. 총 판매금액을 리턴하는 메소드 (가격*수량)
+	public int totalPrice() {
+		int total = this.price * this.quantity;
+		
+		return total;
+	}
+	
+	// 4. 내용이 20자를 초과하면
+	// ex) 12345678901234567890... 으로 변환하는 메소드
+	public void checkContent() {
+		String[] str = this.content.split("");
+		
+		if (this.content.length() > 20) {
+			for (int i=0; i<20; i++) {
+				System.out.print(str[i]);
+			}
+			System.out.print("...");
+			System.out.println();
+		}
+		else {
+			System.out.println(this.content);
+		}
+	}
+	
+	// 5. 이름을 뒤집어서 반환하는 메소드 ex) 가나다 -> 다나가
+	public void reverseName() {
+		String[] str = this.name.split("");
+		
+		for (int i=str.length; i>0; i--) {
+			System.out.print(str[i-1]);
+		}
+	}
+	
+	public String revName() {
+		String str = ""; // 문자를 누적할 변수
+		
+		for (int i=this.name.length()-1; i>=0; i--) {
+			char res = this.name.charAt(i);
+			str = str + res;
+		}
+		
+		return str;
+	}
 }
--- src/day4/ItemMain.java
+++ src/day4/ItemMain.java
@@ -1,10 +1,32 @@
 package day4;
 
+import java.util.Date;
+
 public class ItemMain {
 
 	public static void main(String[] args) {
+		Member member = new Member("abcdefg", "hijklmn", "신애린",
+				"010-9896-1402", "D", new Date());
+		Item item = new Item(5555, "물품명", "123456789012345678901111", 
+								5000, 5, member);
+		System.out.println(item.toString());
 		
-
+		// 1. 수량이 n개 미만이면 n개로 변경
+		System.out.println(item.underCount(20));
+		
+		// 2. 가격에 할인율만큼 빼서 리턴
+		System.out.println(item.percentPrice(0.1f));
+		System.out.println(item.toString());
+		
+		// 3. 총 판매금액을 리턴
+		System.out.println(item.totalPrice());
+		
+		// 4. 내용이 20자 이상이면 변환
+		item.checkContent();
+		
+		// 5. 이름을 뒤집어서 반환
+		item.reverseName();
+		System.out.println(item.revName());
 	}
 
 }
